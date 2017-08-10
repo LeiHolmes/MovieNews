beff06c7b0522d40bfc931def3a1ef4fdabf0a6d
LeiHolmes <xulei964270141@qq.com>
2017-08-10 17:46:51 +0800

    Toolbar+Drawerlayout

--- app/src/main/java/com/holmeslei/movienews/ui/activity/MainActivity.java
+++ app/src/main/java/com/holmeslei/movienews/ui/activity/MainActivity.java
@@ -1,9 +1,18 @@
 package com.holmeslei.movienews.ui.activity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
 
 import com.holmeslei.movienews.R;
@@ -15,6 +24,7 @@ import com.holmeslei.movienews.ui.adapter.ShowingMoviesAdapter;
 import com.holmeslei.movienews.ui.base.BaseActivity;
 
 import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * Description:   入口Activity
@@ -24,6 +34,13 @@ import butterknife.BindView;
 public class MainActivity extends BaseActivity implements MainView {
     @BindView(R.id.rv_Main)
     RecyclerView rvMain;
+    @BindView(R.id.tb_base)
+    Toolbar tbBase;
+    @BindView(R.id.nav_main)
+    NavigationView navMain;
+    @BindView(R.id.dl_main)
+    DrawerLayout dlMain;
+    private ActionBarDrawerToggle toggle;
     private MainPresenter mainPresenter;
     private ShowingMoviesAdapter adapter;
 
@@ -40,11 +57,71 @@ public class MainActivity extends BaseActivity implements MainView {
 
     @Override
     protected void onCreateActivity(Bundle savedInstanceState) {
+        initToolBar();
+        initNavigationView();
+        initRecyclerView();
+        initData();
+    }
+
+    private void initToolBar() {
+        tbBase.setTitle("影讯");
+        tbBase.setTitleTextColor(Color.parseColor("#ffffff"));
+        setSupportActionBar(tbBase);
+        getSupportActionBar().setHomeButtonEnabled(true); //设置返回键可用 
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true); //加返回箭头
+        toggle = new ActionBarDrawerToggle(this, dlMain, tbBase, R.string.open, R.string.close) {
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+            }
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+            }
+        };
+        toggle.syncState();
+        dlMain.setDrawerListener(toggle);
+    }
+
+    private void initNavigationView() {
+        navMain.setItemIconTintList(null); //设置图标为彩色，否则都为灰色
+        navMain.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                item.setChecked(true);
+                dlMain.closeDrawer(GravityCompat.START);
+                tbBase.setTitle(item.getTitle().toString());
+                switch (item.getItemId()) {
+                    case R.id.menu_nav_news:
+                        tbBase.setTitle(item.getTitle().toString());
+                        break;
+                    case R.id.menu_nav_comments:
+                        break;
+                    case R.id.menu_nav_actors:
+                        break;
+                    case R.id.menu_nav_search:
+                        break;
+                    case R.id.menu_nav_setting:
+                        break;
+                    case R.id.menu_nav_about:
+                        break;
+                }
+                return true;
+            }
+        });
+    }
+
+    private void initData() {
+        //请求北京正在热映电影的数据
+        mainPresenter.requestShowingMovies("北京");
+    }
+
+    private void initRecyclerView() {
         adapter = new ShowingMoviesAdapter(this);
         rvMain.setAdapter(adapter);
         rvMain.setHasFixedSize(true);
         rvMain.setLayoutManager(new LinearLayoutManager(this));
-        mainPresenter.requestShowingMovies("北京");
     }
 
     @Override

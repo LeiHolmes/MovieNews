c12f2c949ec0c9ead4973719001a70176a44bdaf
xulei <xulei964270141@qq.com>
2017-08-14 15:35:17 +0800

    SwipeRefreshLayout

--- app/src/main/java/com/holmeslei/movienews/ui/adapter/MainViewPagerAdapter.java
+++ app/src/main/java/com/holmeslei/movienews/ui/adapter/MainViewPagerAdapter.java
@@ -1,6 +1,5 @@
 package com.holmeslei.movienews.ui.adapter;
 
-import android.content.Context;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
--- app/src/main/java/com/holmeslei/movienews/ui/fragment/GeneralMovieFragment.java
+++ app/src/main/java/com/holmeslei/movienews/ui/fragment/GeneralMovieFragment.java
@@ -8,6 +8,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
 import android.widget.Toast;
 
 import com.holmeslei.movienews.R;
@@ -16,6 +17,8 @@ import com.holmeslei.movienews.mvp.presenter.GeneralFragPresenter;
 import com.holmeslei.movienews.mvp.view.GeneralFragView;
 import com.holmeslei.movienews.ui.adapter.ShowingMoviesAdapter;
 import com.holmeslei.movienews.ui.base.BaseFragment;
+import com.holmeslei.movienews.ui.widget.swipe.SwipeRefreshLayout;
+import com.holmeslei.movienews.ui.widget.swipe.SwipeRefreshLayoutDirection;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -31,6 +34,8 @@ public class GeneralMovieFragment extends BaseFragment<GeneralFragPresenter> imp
     public static final String PARAM = "param";
     @BindView(R.id.rv_general_movie)
     RecyclerView rvGeneralMovie;
+    @BindView(R.id.srl_general_movie)
+    SwipeRefreshLayout srlGeneralMovie;
     private View view;
     private String function; //功能
     private String param; //参数
@@ -63,6 +68,13 @@ public class GeneralMovieFragment extends BaseFragment<GeneralFragPresenter> imp
     private void initData() {
         function = getArguments().getString(FUNCTION);
         param = getArguments().getString(PARAM);
+        requestData();
+    }
+
+    /**
+     * 请求网络数据
+     */
+    private void requestData() {
         switch (function) {
             case "影讯":
                 generalFragPresenter.requestShowingMovies(param, "北京");
@@ -80,10 +92,55 @@ public class GeneralMovieFragment extends BaseFragment<GeneralFragPresenter> imp
      * 初始化RecyclerView
      */
     private void initRecyclerView() {
+        //RecyclerView配置
         adapter = new ShowingMoviesAdapter(getActivity());
         rvGeneralMovie.setAdapter(adapter);
         rvGeneralMovie.setHasFixedSize(true);
         rvGeneralMovie.setLayoutManager(new LinearLayoutManager(getActivity()));
+        //设置初始为刷新状态
+        srlGeneralMovie.post(new Runnable() {
+            @Override
+            public void run() {
+                setSwipeRefreshLoadingState();
+            }
+        });
+        srlGeneralMovie.setDirection(SwipeRefreshLayoutDirection.BOTH);//同时支持上拉和下拉
+        srlGeneralMovie.setColorSchemeResources(R.color.colorAccent, R.color.bg_blue, R.color.orange_light,
+                R.color.red_light);
+        srlGeneralMovie.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh(SwipeRefreshLayoutDirection direction) {
+                setSwipeRefreshLoadingState();
+                switch (direction) {
+                    case TOP:
+                        requestData();
+                        break;
+                    case BOTTOM:
+                        break;
+                }
+            }
+        });
+    }
+
+    /**
+     * 设置顶部正在加载的状态
+     */
+    private void setSwipeRefreshLoadingState() {
+        if (srlGeneralMovie != null) {
+            srlGeneralMovie.setRefreshing(true);
+            // 防止多次重复刷新
+            srlGeneralMovie.setEnabled(false);
+        }
+    }
+
+    /**
+     * 设置顶部加载完毕的状态
+     */
+    private void setSwipeRefreshLoadedState() {
+        if (srlGeneralMovie != null) {
+            srlGeneralMovie.setRefreshing(false);
+            srlGeneralMovie.setEnabled(true);
+        }
     }
 
     @Override
@@ -92,14 +149,22 @@ public class GeneralMovieFragment extends BaseFragment<GeneralFragPresenter> imp
         return generalFragPresenter;
     }
 
+    /**
+     * 获得电影数据
+     */
     @Override
     public void getShowingMoviesData(ShowingMovies showingMovies) {
         Log.i("getShowingMoviesData", showingMovies.toString());
         adapter.setData(showingMovies.getSubjects());
+        setSwipeRefreshLoadedState();
     }
 
+    /**
+     * 获得电影数据请求错误日志
+     */
     @Override
     public void getShowingMoviesError(String errorMessage) {
         Log.i("getShowingMoviesError", errorMessage);
+        setSwipeRefreshLoadedState();
     }
 }
